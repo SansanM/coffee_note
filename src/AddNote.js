b103c8961fa260914572865b34cca635a9143715
SansanM <52972918+SansanM@users.noreply.github.com>
2020-11-17 01:26:40 +0900

    addNote complete

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,12 +1,111 @@
 import Axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react';
 import { withCookies } from 'react-cookie';
+import { Link } from "react-router-dom";
+import { Button, TextField } from '@material-ui/core/';
 
-const AddNote = (props) =>{
+import Formstyle from "./css/AddNoteForm.module.css"
+import style from "./css/pageTitle.module.css"
 
-    return(
+import { makeStyles } from '@material-ui/core/styles';
+
+//フォーム同士の隙間の設定
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+//新規noteの投稿
+const AddNote = (props) => {
+    const [title, setTitle] = useState("");
+    const [note, setNote] = useState("");
+    const [sanmi, setSanmi] = useState(3);
+    const [nigami, setNigami] = useState(3);
+    const [like, setLike] = useState(3);
+    const classes = useStyles();
+
+
+    //投稿の非同期通信の処理
+    const NotePost = () =>{
+
+        const token = props.cookies.get('coffeeNote-token');
+        const url = "http://127.0.0.1:8000/note/note/";
+        
+        let form_data = new FormData();
+        form_data.append('title', title);
+        form_data.append('body', note);
+        form_data.append('sanmi', sanmi);
+        form_data.append('nigami', nigami);
+        form_data.append('like', like);
+
+        Axios.post(url,form_data, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "jwt " + token,
+            },
+        })
+        .then( res => {
+            console.log(res)
+            window.location.href = "/";
+        })
+        .catch( e =>{console.log(e)})
+
+    } 
+
+
+
+
+    return (
         <div>
-            
+            <div className={style.titlearea}>
+                <h2 className={style.title}>Add Note</h2>
+                <Link className={style.newNote} to={"/home"}>
+                    <Button>
+                        My Note
+                    </Button>
+                </Link>
+            </div>
+            <div className={Formstyle.root +" "+ classes.root}>
+                <TextField
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    label="タイトル"
+                    variant="outlined"
+                /><br />
+                <TextField
+                    onChange={(e) => setSanmi(e.target.value)}
+                    value={sanmi}
+                    type="number"
+                    label="酸味"
+                    variant="outlined"
+                /><br />
+                 <TextField
+                    onChange={(e) => setNigami(e.target.value)}
+                    value={nigami}
+                    type="number"
+                    label="苦味"
+                    variant="outlined"
+                /><br />
+                 <TextField
+                    onChange={(e) => setLike(e.target.value)}
+                    value={like}
+                    type="number"
+                    label="評価"
+                    variant="outlined"
+                /><br />
+                 <TextField
+                    onChange={(e) => setNote(e.target.value)}
+                    value={note}
+                    label="メモ"
+                    variant="outlined"
+                /><br />
+                <Button onClick ={NotePost}>
+                    投稿する
+                </Button>
+            </div>
         </div>
     )
 }
--- src/component/NotesComponent.js
+++ src/component/NotesComponent.js
@@ -1,24 +1,31 @@
-import PrintNoteData from "./PrintNoteData"
 import React from 'react'
+import {Link} from "react-router-dom";
 import { withCookies } from 'react-cookie';
+import { Button } from '@material-ui/core/';
+import style from "../css/pageTitle.module.css"
 
+import PrintNoteData from "./PrintNoteData"
 
-const NotesComponent = (props) =>{
-    
-    return(
+const NotesComponent = (props) => {
+    return (
         <div>
-            <h2>My Note</h2>
+            <div className={style.titlearea}>
+                <h2 className={style.title}>My Note</h2>
+                <Link className={style.newNote} to={"/AddNote"}>
+                    <Button>
+                        Add Note
+                    </Button>
+                </Link>
+
+            </div>
             {props.notesData.map(
-                (data)=>{
-                    return(
-                        <React.Fragment>
-                            
-                            <PrintNoteData noteData={data}/>
-                        </React.Fragment>
+                (data) => {
+                    return (
+                        <PrintNoteData noteData={data} key={data.uuid} />
                     )
                 }
             )}
-          
+
         </div>
     )
 }
